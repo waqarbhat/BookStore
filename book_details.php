83ee7e2ccd4b023de691aa1beb40dc634690a29f
waqarbhat <55852828+waqarbhat@users.noreply.github.com>
2020-10-01 14:30:54 +0530

    Update book_details.php

--- book_details.php
+++ book_details.php
@@ -4,7 +4,6 @@
         echo "<script type='text/javascript' src='includes/javascript/ratings.js'></script>";
         echo "<script type='text/javascript' src='includes/javascript/book_details.js'></script>";
         echo "<script type='text/javascript' src='includes/javascript/reviews.js'></script>";
-        // echo '<script src="https://cdn.ckeditor.com/ckeditor5/11.0.1/classic/ckeditor.js"></script>';
     }
 ?>
 <?php include "./templates/header.php"; ?>
